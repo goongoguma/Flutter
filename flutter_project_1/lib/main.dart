22490e3525e0de67f33bf1888d68ac8b991f6767
JaeHyun <wogus7an@gmail.com>
2022-03-30 20:04:43 +0900

    widget나누기, get 사용, widget 나누기

--- flutter_project_1/lib/main.dart
+++ flutter_project_1/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_project_1/answer.dart';
+import 'package:flutter_project_1/result.dart';
 
 import './question.dart';
 
-import './question.dart';
+import './quiz.dart';
+import './result.dart';
 
 void main() => runApp(MyApp());
 
@@ -21,17 +23,35 @@ class MyApp extends StatefulWidget {
 // private 클래스로 만들어주기 위해서 _를 붙이는것이 관례
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  // const -> compile time constant value 
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  // const -> compile time constant value
   // 이미 값이 고정되어있음
-  static const questions = [
-    {'questionText': 'What\'s is your favorite color?', 'answers': ['black', 'red', 'green', 'white', 'blue']}, 
-    {'questionText': 'What\'s your favorite animal?', 'answers': ['tiger','lion', 'ox', 'snake', 'rhino']},
-    {'questionText': 'What\'s your favorite food?', 'answers': ['김치', '갈비', '초밥', '치킨', '순대국']},
+  static const _questions = [
+    {
+      'questionText': 'What\'s is your favorite color?',
+      'answers': [{'text': 'black', 'score': 10}, {'text': 'red', 'score': 5}, {'text':'green', 'score': 7}, {'text':'white', 'score': 1}, {'text':'blue', 'score': 1}]
+    },
+    {
+      'questionText': 'What\'s your favorite animal?', 
+      'answers': [{'text':'tiger', 'score': 5}, {'text':'lion', 'score': 2}, {'text':'ox', 'score': 1}, {'text': 'snake', 'score': 2}, {'text': 'rhino', 'score': 9}]
+    },
+    {
+      'questionText': 'What\'s your favorite food?',
+      'answers': [{'text': '김치', 'score': 10}, {'text': '갈비', 'score': 7}, {'text': '초밥', 'score': 4}, {'text': '치킨', 'score': 1}, {'text': '순대국', 'score': 1}]
+    },
   ];
-  
-  void _answerQuestion() {
-    if (_questionIndex < questions.length) {
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
+    if (_questionIndex < _questions.length) {
       print('we have more questions');
     }
     // setState는 build 메서드를 호출하고 build 메서드는 위젯트리를 다시 빌드한다
@@ -40,6 +60,7 @@ class _MyAppState extends State<MyApp> {
     });
     print(_questionIndex);
   }
+
   // @override 데코레이터는 위젯에 의해 제공된 build 메서드를 override 했다고 명시하기 위해 사용한것. 통상적으로 쓰임
   @override
   // build 메서드를 사용해 MaterialApp 위젯을 리턴하고 context에서 위젯 트리에서의 위치 정보를 확인할 수 있다
@@ -48,14 +69,15 @@ class _MyAppState extends State<MyApp> {
     // return MaterialApp(home: Text('Hello!'),);
 
     // Scaffold를 사용해서 앱에서 사용할 기본 디자인을 생성한다
-    return MaterialApp(home: Scaffold(
-      appBar: AppBar(title: Text('My First App'),),
-      body: Column(children: [
-        Question( questions[_questionIndex]['questionText']), 
-        ...(questions[_questionIndex]['answers'] as List<String>).map((answer) {
-          return Answer(_answerQuestion, answer);
-        }).toList()
-      ]),
-    ),);
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(answerQuestion: _answerQuestion, questionIndex: _questionIndex, questions: _questions) 
+            : Result(_totalScore, _resetQuiz),
+      ),
+    );
   }
 }
---
+++ flutter_project_1/lib/result.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class Result extends StatelessWidget {
+  final int resultScore;
+  final Function resetHandler;
+
+  Result(this.resultScore, this.resetHandler);
+
+  // getter (value is calculated dynamically)
+  String get resultPhrase {
+    String resultText;
+    if (resultScore <= 8) {
+      resultText = 'You are awesome!';
+    } else if (resultScore <= 12) {
+      resultText = 'Pretty likable!';
+    } else if (resultScore <= 16) {
+      resultText = 'You are... Strange';
+    } else {
+      resultText = 'You are so bad';
+    }
+    ;
+    return resultText;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Column(children: <Widget>[
+      Text(
+        resultPhrase,
+        style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+        textAlign: TextAlign.center,
+      ),
+      FlatButton(child: Text('Restart Quiz!'), textColor: Colors.blue, onPressed: resetHandler,)
+    ]));
+  }
+}
