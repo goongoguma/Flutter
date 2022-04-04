be7b0abd923e23e1d97742cc391594d6d1af4eca
JaeHyun <wogus7an@gmail.com>
2022-04-04 09:36:04 +0900

    Listview.builder

--- flutter_project_2_my_expenses/lib/widgets/transaction_list.dart
+++ flutter_project_2_my_expenses/lib/widgets/transaction_list.dart
@@ -11,28 +11,27 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: SingleChildScrollView(
-        child: Column(
-          children: transactions.map((tx) {
-          return Card(
+        child: ListView.builder(
+          itemBuilder: (ctx, index) {
+            return Card( 
             child: Row(children: <Widget>[
               Container(
                 margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
                 decoration: BoxDecoration(border: Border.all(color: Colors.purple, width: 2)),
                 padding: EdgeInsets.all(10),
-                child: Text('\$${tx.amount.toString()}', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.purple))
+                child: Text('\$${transactions[index].amount.toString()}', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.purple))
               ),
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                Text(tx.title, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
-                Text(DateFormat('yyyy/MM/dd').format(tx.date), style: TextStyle(color: Colors.grey)),
+                Text(transactions[index].title, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
+                Text(DateFormat('yyyy/MM/dd').format(transactions[index].date), style: TextStyle(color: Colors.grey)),
                 ],)
               ],)
             );
-          }).toList()
+          },
+          itemCount: transactions.length,
         )
-      )
     );
   }
 }
