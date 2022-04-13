49dd984294464e6d2cc22bacd4e238281264c499
JaeHyun <wogus7an@gmail.com>
2022-04-13 20:25:53 +0900

    decoration

--- flutter_project_2_my_expenses/lib/main.dart
+++ flutter_project_2_my_expenses/lib/main.dart
@@ -30,22 +30,40 @@ class MyHomePage extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Container(
-            width: double.infinity,
-            child: Card(
-              color: Colors.blue,
-              child: Text('CHART!'),
-              elevation: 5,
-            )
-          ),
-          Column(children: transactions.map((tx) {
-            return Card(child: 
-            Row(children: [
-              Container(child: Text(tx.amount.toString(),)),
-            Column(children: [
-              Text(tx.title),
-              Text(tx.date.toString()),
-            ],)
-            ]),);
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text('CHART!'),
+                elevation: 5,
+              )),
+          Column(
+              children: transactions.map((tx) {
+            return Card(
+              child: Row(children: [
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.purple, width: 2)),
+                  child: Text(tx.amount.toString(),
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple,
+                      )),
+                  padding: EdgeInsets.all(10),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(tx.title, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                    Text(
+                      tx.date.toString(),
+                      style: TextStyle(fontSize: 12, color: Colors.grey),
+                    ),
+                  ],
+                )
+              ]),
+            );
           }).toList()),
         ],
       ),
