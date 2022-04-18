5f1f065b6cd62aba4b18e12046491ca1d463cf3e
JaeHyun <wogus7an@gmail.com>
2022-04-18 20:36:20 +0900

    SingleChildScrollView

--- flutter_project_2_my_expenses/lib/main.dart
+++ flutter_project_2_my_expenses/lib/main.dart
@@ -20,23 +20,24 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter App'),
-      ),
-      body: Column(
-        // mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.blue,
-                child: Text('CHART!'),
-                elevation: 5,
-              )),
-          UserTransaction()
-        ],
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Flutter App'),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                  width: double.infinity,
+                  child: Card(
+                    color: Colors.blue,
+                    child: Text('CHART!'),
+                    elevation: 5,
+                  )),
+              UserTransaction()
+            ],
+          ),
+        ));
   }
 }
--- flutter_project_2_my_expenses/lib/widget/transaction_list.dart
+++ flutter_project_2_my_expenses/lib/widget/transaction_list.dart
@@ -9,37 +9,42 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return (Column(
-        children: transactions.map((tx) {
-      return Card(
-        child: Row(children: [
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.purple, width: 2)),
-            child: Text('\$${tx.amount}',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                  color: Colors.purple,
-                )),
-            padding: EdgeInsets.all(10),
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                tx.title,
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              ),
-              Text(
-                DateFormat.yMMMd().format(tx.date),
-                style: TextStyle(fontSize: 12, color: Colors.grey),
-              ),
-            ],
-          )
-        ]),
-      );
-    }).toList()));
+    return (Container(
+        height: 300,
+        child: SingleChildScrollView(
+          child: Column(
+              children: transactions.map((tx) {
+            return Card(
+              child: Row(children: [
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.purple, width: 2)),
+                  child: Text('\$${tx.amount}',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple,
+                      )),
+                  padding: EdgeInsets.all(10),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      tx.title,
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      DateFormat.yMMMd().format(tx.date),
+                      style: TextStyle(fontSize: 12, color: Colors.grey),
+                    ),
+                  ],
+                )
+              ]),
+            );
+          }).toList()),
+        )));
   }
 }
