c7bf3af3efe8b211e33a3684fc9bd21a4eecea99
JaeHyun <wogus7an@gmail.com>
2022-04-18 20:58:04 +0900

    ListView vs ListView.builder

--- flutter_project_2_my_expenses/lib/widget/transaction_list.dart
+++ flutter_project_2_my_expenses/lib/widget/transaction_list.dart
@@ -10,17 +10,16 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return (Container(
-        height: 300,
-        child: SingleChildScrollView(
-          child: Column(
-              children: transactions.map((tx) {
+      height: 300,
+      child: ListView.builder(
+          itemBuilder: (ctx, index) {
             return Card(
               child: Row(children: [
                 Container(
                   margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
                   decoration: BoxDecoration(
                       border: Border.all(color: Colors.purple, width: 2)),
-                  child: Text('\$${tx.amount}',
+                  child: Text('\$${transactions[index].amount}',
                       style: TextStyle(
                         fontWeight: FontWeight.bold,
                         fontSize: 20,
@@ -32,19 +31,21 @@ class TransactionList extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
-                      tx.title,
+                      transactions[index].title,
                       style:
                           TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                     ),
                     Text(
-                      DateFormat.yMMMd().format(tx.date),
+                      DateFormat.yMMMd().format(transactions[index].date),
                       style: TextStyle(fontSize: 12, color: Colors.grey),
                     ),
                   ],
                 )
               ]),
             );
-          }).toList()),
-        )));
+          },
+          itemCount: transactions.length,
+        ),
+    ));
   }
 }
