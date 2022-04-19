82618d179a8b599d7152ca7a1190c4e0a45c71fa
JaeHyun <wogus7an@gmail.com>
2022-04-19 20:49:56 +0900

    모달추가 및 위젯 구조 수정

--- flutter_project_2_my_expenses/lib/main.dart
+++ flutter_project_2_my_expenses/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import './widget/user_transaction.dart';
+import 'package:flutter_complete_guide/widget/transaction_list.dart';
+import './widget/new_transction.dart';
+import './model/transaction.dart';
+import './widget/transaction_list.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,35 +16,75 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   // String titleInput;
-  // String amountInput;
+  final List<Transaction> _userTransactions = [
+    Transaction(id: 't1', title: '맥북', amount: 19.99, date: DateTime.now()),
+    Transaction(id: 't2', title: '아이폰', amount: 25.99, date: DateTime.now()),
+  ];
+
+  void _addNewTransaction(String txTitle, double txAmount) {
+    final newTx = Transaction(
+        title: txTitle,
+        amount: txAmount,
+        date: DateTime.now(),
+        id: DateTime.now().toString());
+
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+        context: ctx,
+        builder: (_) {
+          return GestureDetector(
+            onTap: () {},
+            child: NewTransaction(_addNewTransaction),
+            // HitTestBehavior.opaqu을 사용하면 화면을 tab했을때 modal이 무조건 꺼지는것을 방지할 수 있음
+            behavior: HitTestBehavior.opaque,
+          );
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter App'),
-          actions: [IconButton(onPressed: () {}, icon: Icon(Icons.add))],
+      appBar: AppBar(
+        title: Text('Flutter App'),
+        actions: [
+          IconButton(
+              onPressed: () => _startAddNewTransaction(context),
+              icon: Icon(Icons.add))
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+                width: double.infinity,
+                child: Card(
+                  color: Colors.blue,
+                  child: Text('CHART!'),
+                  elevation: 5,
+                )),
+            TransactionList(_userTransactions)
+          ],
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            // mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Container(
-                  width: double.infinity,
-                  child: Card(
-                    color: Colors.blue,
-                    child: Text('CHART!'),
-                    elevation: 5,
-                  )),
-              UserTransaction()
-            ],
-          ),
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-        floatingActionButton: FloatingActionButton(child: Icon(Icons.add), onPressed: () {},),
-      );
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => _startAddNewTransaction(context),
+      ),
+    );
   }
 }
