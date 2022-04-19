8c1688383d50c1031d8f930b2d665537f359044e
JaeHyun <wogus7an@gmail.com>
2022-04-19 21:18:20 +0900

    모달 만들기

--- my_playground/lib/main.dart
+++ my_playground/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'widget/user_transaction.dart';
+import 'package:flutter_complete_guide/widget/transaction_list.dart';
+import 'model/transaction.dart';
+import 'widget/transaction_form.dart';
 // What is the difference between SingleChildScrollView ListView and ListView builder?
 // https://stackoverflow.com/questions/62146197/what-is-the-difference-between-listview-and-singlechildscrollview-in-flutter#:~:text=SingleChildScrollView%3A%20is%20used%20when%20we%20have%20different%20widgets%20for%20n%20elements.&text=ListView()%20%2D%2D%20Render%20all,visible%20items%20on%20the%20screen.
 
@@ -15,28 +17,76 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> transactions = [
+    Transaction(id: 'id1', title: '운동화', amount: 45000, date: DateTime.now()),
+    Transaction(id: 'id2', title: '핸드폰', amount: 130000, date: DateTime.now()),
+  ];
+
+  void _addTransactions(String title, double amount) {
+    final newItem = Transaction(
+        id: DateTime.now().toString(),
+        title: title,
+        amount: amount,
+        date: DateTime.now());
+
+    print(title);
+
+    setState(() {
+      transactions.add(newItem);
+    });
+  }
+
+  void _startAddNewTransactionModal(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (_) {
+          return GestureDetector(
+            onTap: () {},
+            child: TransactionForm(_addTransactions),
+            behavior: HitTestBehavior.opaque,
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('Flutter App')),
-        body: SingleChildScrollView(
-          child: Column(
-            // mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              // Column에서 Card의 넓이나 높이를 조절하고 싶으면 Container로 감싸야함
-              // 대신 Column안에 있는 아이템들의 위치는 조절 가능
-              Container(
-                  width: double.infinity,
-                  child: Card(
-                    color: Colors.blue,
-                    child: Text('CHART!'),
-                    elevation: 5,
-                  )),
-              UserTransaction(),
-            ],
-          ),
-        ));
+      appBar: AppBar(
+        title: Text('Flutter App'),
+        actions: [
+          IconButton(
+              onPressed: () => _startAddNewTransactionModal(context),
+              icon: Icon(Icons.add))
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            // Column에서 Card의 넓이나 높이를 조절하고 싶으면 Container로 감싸야함
+            // 대신 Column안에 있는 아이템들의 위치는 조절 가능
+            Container(
+                width: double.infinity,
+                child: Card(
+                  color: Colors.blue,
+                  child: Text('CHART!'),
+                  elevation: 5,
+                )),
+            TransactionList(transactions)
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => _startAddNewTransactionModal(context),
+      ),
+    );
   }
 }
