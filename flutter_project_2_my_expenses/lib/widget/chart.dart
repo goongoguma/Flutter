edfff4af4f4a372f908c22724c861c8eccb6d3d5
JaeHyun <wogus7an@gmail.com>
2022-04-23 14:18:25 +0900

    chart bar 그래프 추가

--- flutter_project_2_my_expenses/lib/widget/chart.dart
+++ flutter_project_2_my_expenses/lib/widget/chart.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/widget/chart_bar.dart';
 import 'package:intl/intl.dart';
 import 'package:flutter_complete_guide/model/transaction.dart';
 import '../model/transaction.dart';
+import '../widget/chart_bar.dart';
 
 class Chart extends StatelessWidget {
   final List<Transaction> recentTransactions;
@@ -21,7 +23,17 @@ class Chart extends StatelessWidget {
         }
       }
 
-      return {'day': DateFormat.E().format(weekDay).substring(0, 1), 'amount': totalSum};
+      return {
+        'day': DateFormat.E().format(weekDay).substring(0, 1),
+        'amount': totalSum
+      };
+    });
+  }
+
+  double get totalSpending {
+    // fold(dart) = reduce(js)
+    return groupedTransactionValues.fold(0, (sum, item) {
+      return sum + item['amount'];
     });
   }
 
@@ -32,7 +44,11 @@ class Chart extends StatelessWidget {
       margin: EdgeInsets.all(20),
       child: Row(
         children: groupedTransactionValues.map((data) {
-          return Text('${data['day']}: ${data['amount']}');
+          return ChartBar(
+              data['day'],
+              data['amount'],
+              totalSpending ==
+                  0 ? 0 : (data['amount'] as double) / totalSpending);
         }).toList(),
       ),
     );
