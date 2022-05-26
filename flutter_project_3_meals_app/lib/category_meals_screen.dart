fad64f0077cc9672304b277d5f6ab7ba0a89f29a
JaeHyun <wogus7an@gmail.com>
2022-05-26 19:39:13 +0900

    where과 contains를 사용해 id로 데이터 찾기

--- flutter_project_3_meals_app/lib/category_meals_screen.dart
+++ flutter_project_3_meals_app/lib/category_meals_screen.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
+import './dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/category-meals';
-  // final String categoryId;
-  // final String categoryTitle;
-
-  // CategoryMealsScreen(this.categoryId, this.categoryTitle);
 
   @override
   Widget build(BuildContext context) {
@@ -13,13 +10,19 @@ class CategoryMealsScreen extends StatelessWidget {
         ModalRoute.of(context)!.settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'];
     final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle as String),
       ),
-      body: ListView.builder(itemBuilder: (ctx, index) {
-
-      }, itemCount: ,),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Text(categoryMeals[index].title);
+        },
+        itemCount: categoryMeals.length,
+      ),
     );
   }
 }
