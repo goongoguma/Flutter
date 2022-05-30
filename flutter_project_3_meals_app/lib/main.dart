9167f1adf1ef9e61acadca156b186117b0137440
JaeHyun <wogus7an@gmail.com>
2022-05-31 06:34:08 +0900

    DefaultTabController로 탭 추가하기

--- flutter_project_3_meals_app/lib/main.dart
+++ flutter_project_3_meals_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screens/tabs_screen.dart';
 import 'screens/meal_detail_screen.dart';
 import 'screens/category_meals_screen.dart';
 import 'screens/categories_screen.dart';
@@ -22,9 +23,10 @@ class MyApp extends StatelessWidget {
                   fontSize: 20,
                   fontFamily: 'RobotoCondensed',
                   fontWeight: FontWeight.bold))),
-      home: CategoriesScreen(),
+      // home: CategoriesScreen(),
       initialRoute: '/', // default is '/,
       routes: {
+        '/': (context) => TabsScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen()
       },
