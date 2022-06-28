8bfef59a574fdf91022c9607a6a75f837816c230
JaeHyun <wogus7an@gmail.com>
2022-06-28 19:05:36 +0900

    스타일링

--- flutter_project_4_shopping/lib/main.dart
+++ flutter_project_4_shopping/lib/main.dart
@@ -14,8 +14,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'My Shop',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+          fontFamily: 'Lato'),
       home: ProduceOverviewScreen(),
     );
   }
