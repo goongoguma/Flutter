6ebaf5f595c221c3f223a8da43275f5497adbb38
JaeHyun <wogus7an@gmail.com>
2022-06-29 21:06:51 +0900

    개별 상태 내려주기

--- flutter_project_4_shopping/lib/provider/products.dart
+++ flutter_project_4_shopping/lib/provider/products.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/foundation.dart';
-import '../models/product.dart';
+import 'product.dart';
 
 class Products with ChangeNotifier {
   final List<Product> _items = [
@@ -39,6 +39,10 @@ class Products with ChangeNotifier {
 
   List<Product> get items {
     return [..._items];
+  }
+
+  Product findById(String id) {
+    return _items.firstWhere((element) => element.id == id);
   }
 
   void addProduct() {
--- flutter_project_4_shopping/lib/screens/products_overview_screen.dart
+++ flutter_project_4_shopping/lib/screens/products_overview_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../models/product.dart';
+import '../provider/product.dart';
 import '../widgets/products_grid.dart';
 
 class ProduceOverviewScreen extends StatelessWidget {
