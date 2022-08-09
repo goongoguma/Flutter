6adbe50a9ababc575b8e40ec392c4c86e94ebe6a
JaeHyun <wogus7an@gmail.com>
2022-08-09 22:30:06 +0900

    데이터 삭제

--- flutter_project_4_shopping/lib/provider/products.dart
+++ flutter_project_4_shopping/lib/provider/products.dart
@@ -73,4 +73,9 @@ class Products with ChangeNotifier {
       print('...');
     }
   }
+
+  void deleteProduct(String id) {
+    _items.removeWhere((element) => element.id == id);
+    notifyListeners();
+  }
 }
