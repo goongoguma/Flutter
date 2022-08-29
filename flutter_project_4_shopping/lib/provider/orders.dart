e4aac9f02852c5f373ca25cc0d0b46a21238a12c
JaeHyun <wogus7an@gmail.com>
2022-08-29 22:20:34 +0900

    주문하기 기능 api 연결

--- flutter_project_4_shopping/lib/provider/orders.dart
+++ flutter_project_4_shopping/lib/provider/orders.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import './cart.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class OrderItem {
   final String id;
@@ -21,13 +23,29 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
-  void addOrder(List<CartItem> cartProducts, double total) {
+  Future<void> addOrder(List<CartItem> cartProducts, double total) async {
+    final url = Uri.https(
+        'flutter-exercise-4b78d-default-rtdb.firebaseio.com', '/orders.json');
+    final timestamp = DateTime.now();
+    final response = await http.post(url,
+        body: json.encode({
+          'amount': total,
+          'dateTime': timestamp.toIso8601String(),
+          'products': cartProducts
+              .map((cp) => {
+                    'id': cp.id,
+                    'title': cp.title,
+                    'quantity': cp.quantity,
+                    'price': cp.price
+                  })
+              .toList()
+        }));
     _orders.insert(
         0,
         OrderItem(
-            id: DateTime.now().toString(),
+            id: json.decode(response.body)['name'],
             amount: total,
-            dateTime: DateTime.now(),
+            dateTime: timestamp,
             products: cartProducts));
     notifyListeners();
   }
